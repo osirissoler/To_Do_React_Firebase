c37ab940cb4fa6de7dcc4cabf5da59821d8b3eb7
osiris soler <osirisascanio02@gmail.com>
2022-03-19 00:22:29 -0400

    fix: icono eliminddo del header

--- src/components/todo/Header.jsx
+++ src/components/todo/Header.jsx
@@ -1,9 +1,9 @@
 export const Header = () => {
     return (
         <div className="index_header text-white row align-items-center justify-content-between">
-            <div className="row ml-1 align-items-center">
-                <i className="fa-solid fa-list-check mr-3"></i>
-                <h6>TO-DO</h6>
+            <div className="mx-2 align-items-center">
+                {/* <i className="fa-solid fa-list-check mr-3"></i> */}
+                <h5>TO-DO</h5>
             </div>
             <h6 className=" mx-2">Osiris Soler Ramirez</h6>
         </div>
