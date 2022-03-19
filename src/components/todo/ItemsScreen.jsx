8dd604f721585fe32c119f8d995ea65a36cd8047
osiris soler <osirisascanio02@gmail.com>
2022-03-19 00:21:55 -0400

    fix:formato de fecha en los card items

--- src/components/todo/ItemsScreen.jsx
+++ src/components/todo/ItemsScreen.jsx
@@ -1,10 +1,12 @@
 import { useDispatch } from "react-redux";
 import { eventActive } from "../../actions/list";
+import moment from 'moment';
 
 
 export const ItemsScreen = ({ id, contenido, titulo, inicio, fin, completada }) => {
 
     const dispatch = useDispatch();
+
 
     const selectList = () => {
         dispatch(eventActive(id, { contenido, titulo, inicio, fin, completada }))
@@ -13,12 +15,14 @@ export const ItemsScreen = ({ id, contenido, titulo, inicio, fin, completada }) 
         <div >
             <div className="card my-1 mx-1 " onClick={selectList}>
                 <div className="card-body">
-                    <h5 className="text-center"><b>{titulo}</b></h5>
-                    <div className="span">{contenido}</div>
+                    <h5 className="card-titulo " ><b>{titulo}</b></h5>
+                    <div className="card-contenido ">
+                        <div>{contenido}</div>
+                    </div>
                 </div>
-                <div className="text-right mx-1 text-muted">
-                    ({inicio})  - ({fin})
-                </div>
+                <small className="text-right my-2 mx-2 text-muted">
+                    {moment(inicio).format('MMMM Do YYYY')}  - {fin?  moment(fin).format('MMMM Do YYYY') :'No especificada'}
+                </small>
             </div>
         </div>
     )
